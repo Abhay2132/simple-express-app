53c8a3e9f78fbcabebf33a94b14d0fbccd2c045a
abhay2132 <abhaybro768@gmail.com>
2022-08-31 20:13:23 +0530

    fs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const {createServer} = require("http");
+const { createServer } = require("http");
 const app = require("express")();
 const port = process.env.PORT || 4000;
+const fs = require("fs")
 
-app.use((req,res) =>{
-	res.end("Hello World | EXPRESS");
+app.use((req, res) => {
+	fs.writeFileSync("data.txt", "Hello World");
+	const hello = fs.readFileSync("data.txt");
+	res.end(hello + " | EXPRESS");
 })
 
 createServer(app)
-.listen(port, ()=>console.log("Server Started at port :", port));
+	.listen(port, () => console.log("Server Started at port :", port));
